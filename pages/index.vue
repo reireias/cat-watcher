c74df64bbca4c97313629f10edbcfb517f05c617
reireias <reireias@gmail.com>
2019-06-19 20:25:49 +0900

    add firebase settings

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-xs-center"></div>
+      <div class="text-xs-center">TODO</div>
     </v-flex>
   </v-layout>
 </template>
