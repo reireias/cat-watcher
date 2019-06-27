845107326d4c8db23df52757043b7dc696860cf2
reireias <reireias@gmail.com>
2019-06-27 10:08:31 +0900

    add snakebar

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,37 @@
 <template>
   <v-layout column justify-center align-center>
     <image-list></image-list>
+
+    <v-snackbar v-model="snakebar" top>
+      {{ notificationBody }}
+      <v-spacer />
+      <v-btn icon @click="snakebar = false"
+        ><v-icon color="primary">cancel</v-icon></v-btn
+      >
+    </v-snackbar>
   </v-layout>
 </template>
 
 <script>
+import firebase from '@/plugins/firebase'
 import ImageList from '@/components/ImageList'
 export default {
   components: {
     ImageList
+  },
+  data() {
+    return {
+      allowed: true,
+      snakebar: false,
+      notificationBody: null
+    }
+  },
+  mounted() {
+    const messaging = firebase.messaging()
+    messaging.onMessage(payload => {
+      this.notificationBody = payload.notification.body
+      this.snakebar = true
+    })
   }
 }
 </script>
